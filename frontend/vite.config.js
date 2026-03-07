463915838caaa9b8b27700dce2a934e399fab139
Lucas_Sartori <lucassartori0604@gmail.com>
2026-03-07 11:14:55 -0300

    corrige erro de digitacao

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/PriceCompatator/",
+  base: "/PriceComparator/",
   plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: ["all"],
